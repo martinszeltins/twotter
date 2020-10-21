8d3232f6eeb332aa75bba4a2e0e1a401f0bbe275
Martins <martins@liga-ubuntu.lv>
2020-10-21 23:29:25 +0300

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,45 @@
 <template>
-    Hello world!
+    Hello, {{ fullName }}!
+
+    <div v-for="user in users" :key="user.id">
+        {{ user.name }}
+    </div>
+
+    <button @click="changeName">
+        Change name
+    </button>
 </template>
 
 <script>
-    export default {
+    import { ref, computed } from 'vue'
 
+    export default {
+        setup(props, { emit })
+        {
+            const firstName = ref('Martins')
+            const lastName = ref('Zeltins')
+            const users = ref([
+                { id: 1, name: 'John' },
+                { id: 2, name: 'Martin' },
+                { id: 3, name: 'Bob' },
+            ])
+
+            const fullName = computed(() => {
+                return firstName.value + ' ' + lastName.value
+            })
+
+            function changeName()
+            {
+                firstName.value = 'Gatis'
+                lastName.value = 'Eisans'
+
+                emit('some-event')
+            }
+
+            return {
+                firstName, lastName, users, changeName, fullName
+            }
+        }
     }
 </script>
 
